a0176df7203837309b991e739b5e22435018221c
Rompelsteeltje <80544110+Rompelsteeltje@users.noreply.github.com>
2021-03-14 22:53:01 +0100

    Add files via upload

---
+++ Project/docentlogin.php
@@ -0,0 +1,29 @@
+<HTML>
+	<head>
+		<title>Docent</title>
+	</head>
+	<body>
+		<div class="input">	
+			<div class="header">
+				<h2>vul je gegevens in<h2>
+			</div>
+			
+			<form action="/Project/docentlogin.php" method="post">
+			<?php include('sql.php') ?>
+			<?php include('errors.php') ?>
+				<div>
+					<label for="naam">naam : </label>
+					<input type="text" name="naam"> 
+				</div>
+				
+				<div>
+					<label for="ww">wachtwoord : </label>
+					<input type="password" name="ww"> 
+				</div>
+				
+				<div>
+					<input type="submit" name="docentLogin">
+				
+			</form>
+		</div>		
+	</body>

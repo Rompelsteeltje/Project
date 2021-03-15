d56eb12f607e3a9503a7ea5aec479688654572cd
Wouter289 <62356516+Wouter289@users.noreply.github.com>
2021-03-15 23:52:52 +0100

    Update vragen.php

--- Project/vragen.php
+++ Project/vragen.php
@@ -3,25 +3,39 @@
 	include('errors.php');
 	
 	if(!isset($_SESSION["count"])){$_SESSION["count"] = 0;}
-	if($_SESSION["count"] > 79){$_SESSION["count"] = 0;}
-	
-	if(isset($_POST[$_SESSION["count"]]))
-	{		
-		storeAnswer($_SESSION['userNumber'], $_SESSION["count"], $_POST[$_SESSION['nameAnswer']]);
-		$_SESSION["count"]++;
+	if($_SESSION["count"] == 79)
+	{
+		$_SESSION["count"] = 0;
+		?>
+		
+		<form action="/Project/Bye.php" method="post">
+			<input type="submit" name="bye" value = "bevestigen">
+		</form>
+		<?php
 	}
-	$_SESSION['nameAnswer'] = "vraag" . $_SESSION["count"];
-	$teller = $_SESSION["count"] +1;
-	echo "Vraag " . $teller;
-?>
+	else
+	{
+		
+		if(isset($_POST[$_SESSION["count"]]))
+		{		
+			storeAnswer($_SESSION['userNumber'], $_SESSION["count"], $_POST[$_SESSION['nameAnswer']]);
+			$_SESSION["count"]++;
+		}
+		$_SESSION['nameAnswer'] = "vraag" . $_SESSION["count"];
+		$teller = $_SESSION["count"] +1;
+		echo "Vraag " . $teller;
+		?>
 
-<form action="/Project/vragen.php" method="post">
-	<a><?php getQuestion($_SESSION["count"]);?></a><br>
-	<input type="radio" name="<?php echo $_SESSION['nameAnswer']; ?>" value="0" required>
-	<label for="<?php echo $_SESSION['nameAnswer']; ?>">Dat is zo</label><br>
-	<input type="radio" name="<?php echo $_SESSION['nameAnswer']; ?>" value="1">
-	<label for="<?php echo $_SESSION['nameAnswer']; ?>">Dat weet ik niet</label><br>
-	<input type="radio" name="<?php echo $_SESSION['nameAnswer']; ?>" value="2">
-	<label for="<?php echo $_SESSION['nameAnswer']; ?>">Dat is niet zo</label><br>
-	<input type="submit" name="<?php echo $_SESSION["count"]; ?>" value = "volgende vraag">
-</form>
+		<form action="/Project/vragen.php" method="post">
+			<a><?php getQuestion($_SESSION["count"]);?></a><br>
+			<input type="radio" name="<?php echo $_SESSION['nameAnswer']; ?>" value="0" required>
+			<label for="<?php echo $_SESSION['nameAnswer']; ?>">Dat is zo</label><br>
+			<input type="radio" name="<?php echo $_SESSION['nameAnswer']; ?>" value="1">
+			<label for="<?php echo $_SESSION['nameAnswer']; ?>">Dat weet ik niet</label><br>
+			<input type="radio" name="<?php echo $_SESSION['nameAnswer']; ?>" value="2">
+			<label for="<?php echo $_SESSION['nameAnswer']; ?>">Dat is niet zo</label><br>
+			<input type="submit" name="<?php echo $_SESSION["count"]; ?>" value = "volgende vraag">
+		</form>
+	<?php
+	}
+	?>
